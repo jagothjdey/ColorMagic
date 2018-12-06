2e5d9a638d00a3b46cb7ee7d6cf277f078bdebeb
jagothjdey <39712897+jagothjdey@users.noreply.github.com>
2018-12-06 19:17:24 +0100

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorMagic
+//
+//  Created by Admin on 06.12.18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
